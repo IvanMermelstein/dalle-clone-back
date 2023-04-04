b4e50c0b78530886c1c352a1a7cf4c325c0c2879
IvanMermelstein <ivan.mermelstein@gmail.com>
2023-04-04 10:10:47 -0300

    Dalle route

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import * as dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import connectDB from './mongodb/connect';
+import postRoutes from './routes/postRoutes';
+import dalleRoutes from './routes/dalleRoutes';
 
 dotenv.config();
 
@@ -9,9 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
-app.get('/', async (req, res) => {
-  res.send('Hello world');
-});
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
 
 const startServer = async () => {
   try {
